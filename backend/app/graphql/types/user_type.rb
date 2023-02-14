0ea81596ee9f10e0689b6549ee82f74f32ed05b1
Yo Kamada <bumpfuji10@gmail.com>
2023-02-15 00:31:21 +0900

    一旦Userモデル作成

--- backend/app/graphql/types/user_type.rb
+++ backend/app/graphql/types/user_type.rb
@@ -2,14 +2,12 @@
 
 module Types
   class UserType < Types::BaseObject
+    description 'ユーザー'
     field :id, ID, null: false
     field :name, String, null: false
     field :email, String, null: false
     field :password, String, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
-    field :name, String
-    field :email, String
-    field :password, String
   end
 end
