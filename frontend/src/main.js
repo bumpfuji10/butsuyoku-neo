50fb74da245b71a343c10f75bcb7efae3c2d4baf
Yo Kamada <bumpfuji10@gmail.com>
2023-02-14 22:42:16 +0900

    setup: vue

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,12 @@
-import { createApp } from "vue";
+import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
 
-createApp(App).use(store).mount("#app");
+Vue.config.productionTip = false;
+
+new Vue({
+  store,
+  render: function (h) {
+    return h(App);
+  },
+}).$mount("#app");
