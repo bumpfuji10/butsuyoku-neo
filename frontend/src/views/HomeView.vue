7ab254dbe4ba8d1f478f78e4cf8eaecca2331745
Yo Kamada <bumpfuji10@gmail.com>
2023-02-27 22:39:54 +0900

    setup: tailwind-css

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,8 +1,8 @@
 <template>
   <!-- <UsersIndex /> -->
   <div>
-    <h1>Gymnastics Tipsへようこそ！</h1>
-    <button>さっそく登録</button>
+    <h1 class="text-2xl italic font-mono">Gymnastics Tipsへようこそ！</h1>
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">ボタン</button>
   </div>
 </template>
 
